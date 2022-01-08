b36a9202562cc60aa872a5eb4748c706c77c2fff
a-ali75 <arjuali75@gmail.com>
2022-01-08 21:19:26 +0530

    Added Dark Mode functionality

--- app/src/main/java/arju/ali/assignments/Assignment1.kt
+++ app/src/main/java/arju/ali/assignments/Assignment1.kt
@@ -1,7 +1,11 @@
 package arju.ali.assignments
 
+import android.content.res.Resources
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import java.util.*
 
 class Assignment1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/arju/ali/assignments/MainActivity.kt
+++ app/src/main/java/arju/ali/assignments/MainActivity.kt
@@ -1,16 +1,99 @@
 package arju.ali.assignments
 
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Switch
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import kotlin.math.log
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var switch: Switch
+    private val defaultValue: Int = 2
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //initialize widgets
+        switch = findViewById<Switch>(R.id.switchThemeMode)
+
+        val appSettingsUIMode: SharedPreferences = getSharedPreferences("AppSettingsUIMode",0)
+        val appSettingsEditor: SharedPreferences.Editor = appSettingsUIMode.edit()
+        val isNightModeOn: Int = appSettingsUIMode.getInt("NightMode",defaultValue)
+
+        if(isNightModeOn==defaultValue)
+        {
+            checkAppThemeMode(appSettingsEditor)
+        }
+        else if(isNightModeOn==1)
+        {
+            switch.isChecked = true
+            switch.text = applicationContext.resources?.getString(R.string.disableDarkMode)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+        else
+        {
+            switch.isChecked = false
+            switch.text = applicationContext.resources?.getString(R.string.enableDarkMode)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
         onClickButtonAssignment1()
+        onClickSwitchMode(appSettingsEditor)
+
     }
+
+    private fun checkAppThemeMode(appSettingsEditor: SharedPreferences.Editor)
+    {
+        when (applicationContext.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK))
+        {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                switch.isChecked = true
+                appSettingsEditor.putInt("NightMode",1)
+                appSettingsEditor.apply()
+                switch.text = applicationContext.resources?.getString(R.string.disableDarkMode)
+
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                switch.isChecked = false
+                appSettingsEditor.putInt("NightMode",0)
+                appSettingsEditor.apply()
+                switch.text = applicationContext.resources?.getString(R.string.enableDarkMode)
+            }
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                switch.isChecked = false
+                appSettingsEditor.putInt("NightMode",0)
+                appSettingsEditor.apply()
+                switch.text = applicationContext.resources?.getString(R.string.enableDarkMode)
+            }
+        }
+    }
+
+    private fun onClickSwitchMode(appSettingsEditor: SharedPreferences.Editor)
+    {
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked)
+            {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                appSettingsEditor.putInt("NightMode",1)
+                appSettingsEditor.apply()
+                switch.text = applicationContext.resources?.getString(R.string.disableDarkMode)
+            }
+            else
+            {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                appSettingsEditor.putInt("NightMode",0)
+                appSettingsEditor.apply()
+                switch.text = applicationContext.resources?.getString(R.string.enableDarkMode)
+            }
+        }
+    }
+
     private fun onClickButtonAssignment1()
     {
         findViewById<Button>(R.id.btnAssignment1).setOnClickListener {
